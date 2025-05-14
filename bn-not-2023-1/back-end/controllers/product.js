0bfdd405c740fe7a24e3bd234debed4dd4cdbf27
Camilli Ramos <88462997+csantos2435@users.noreply.github.com>
2025-05-14 12:42:21 -0300

    Add files via upload
    Projetos

---
+++ bn-not-2023-1/back-end/controllers/product.js
@@ -0,0 +1,97 @@
+// Importação do model
+const Product = require('../models/Product')
+
+const controller = {}   // Objeto vazio
+
+controller.create = async (req, res) => {
+  try {
+    // Manda as informações que vieram em req.body
+    // para serem gravadas no banco de dados
+    await Product.create(req.body)
+
+    // HTTP 201: Created
+    res.status(201).end()
+  }
+  catch(error) {
+    console.error(error)
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
+
+controller.retrieveAll = async (req, res) => {
+  try {
+    // Retorna todos os documentos da coleção
+    const result = await Product.find().populate('supplier')
+    // HTTP 200: OK (implícito)
+    res.send(result)
+  }
+  catch(error) {
+    console.error(error)
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
+
+controller.retrieveOne = async(req, res) => {
+  try {
+    const result = await Product.findById(req.params.id).populate('supplier')
+
+    if(result) {
+      // Encontrou o documento ~> HTTP 200: OK (implícito)
+      res.send(result)
+    }
+    else {
+      // Não encontrou o documento ~> HTTP 404: Not found
+      res.status(404).end()
+    }
+  }
+  catch(error) {
+    console.error(error)
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
+
+controller.update = async (req, res) => {
+  try {
+
+    const result = await Product.findByIdAndUpdate(req.params.id, req.body)
+
+    if(result) {
+      // Encontrou e atualizou ~> HTTP 204: No content
+      res.status(204).end()
+    }
+    else {
+      // Não encontrou para atualizar ~> HTTP 404: Not found
+      res.status(404).end()
+    }
+  }
+  catch(error) {
+    console.error(error)
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
+
+controller.delete = async (req, res) => {
+  try {
+    const result = await Product.findByIdAndDelete(req.params.id)
+
+    if(result) {
+      // Encontrou e excluiu ~> HTTP 204: No content
+      res.status(204).end()
+    }
+    else {
+      // Não encontrou para excluir ~> HTTP 404: Not found
+      res.status(404).end()
+    }
+  }
+  catch(error) {
+    console.error(error)
+    // HTTP 500: Internal Server Error
+    res.status(500).send(error)
+  }
+}
+
+module.exports = controller
---
+++ bn-not-2023-1/back-end/models/Sale.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+const schema = mongoose.Schema({
+  datetime: {
+    type: Date,
+    required: true,
+    default: Date.now()   // Data/hora da gravação
+  },
+  number: {
+    type: Number,
+    required: true
+  },
+  customer: {
+    type: mongoose.ObjectId,  // Tipo para chave estrangeira
+    ref: 'Customer',          // Model estrangeiro
+    required: true
+  },
+  // Vetor de subdocumentos
+  items: [{
+    order: {
+      type: Number,
+      required: true,
+      min: 1
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 0.01
+    },
+    product: {
+      type: mongoose.ObjectId,
+      ref: 'Product',
+      required: true
+    }
+  }]
+})
+
+/*
+  Parâmetros de mongoose.model:
+  1º: nome da model, para uso interno (convenção: primeira letra maiúscula e singular)
+  2º: relação de campos do esquema (constante schema)
+  3º: nome da collection no banco de dados (convenção: mesmo nome do model, mas com
+      letra minúscula e no plural)
+*/
+module.exports = mongoose.model('Sale', schema, 'sales')
